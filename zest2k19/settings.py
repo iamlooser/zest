c40054729a6ce8462ef81a4bd493a9b2a233c1a8
chandu <chandumanikumar5@gmail.com>
2019-03-27 15:35:03 +0530

    icon

--- zest2k19/settings.py
+++ zest2k19/settings.py
@@ -11,9 +11,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6^si6+3nwu6!b3k%39tmda)_2b9-8m#9ar7fesnnbvqsy&e=i='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.73.155.97', '127.0.0.1']
 
 
 # Application definition
